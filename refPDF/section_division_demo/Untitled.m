57a70d0826f134c47ce51b92d4ab72b9dd246d95
linkunda <mcwarmsun@hotmail.com>
2017-11-25 18:53:53 +0800

    charsReflow ing

--- refPDF/section_division_demo/Untitled.m
+++ refPDF/section_division_demo/Untitled.m
@@ -1,11 +1,13 @@
-I=imread('page.jpg');
-imshow(I);
-%画图后：
-h=imrect;%鼠标变成十字，用来选取感兴趣区域
-%图中就会出现可以拖动以及改变大小的矩形框，选好位置后：
-pos=getPosition(h);
-%pos有四个值，分别是矩形框的左下角点的坐标 x y 和 框的 宽度和高度
-%拷贝选取图片
-imCp = imcrop( I, pos );
-figure(2)
-imshow(imCp);
+% I=imread('page.jpg');
+% imshow(I);
+% %画图后：
+% h=imrect;%鼠标变成十字，用来选取感兴趣区域
+% %图中就会出现可以拖动以及改变大小的矩形框，选好位置后：
+% pos=getPosition(h);
+% %pos有四个值，分别是矩形框的左下角点的坐标 x y 和 框的 宽度和高度
+% %拷贝选取图片
+% imCp = imcrop( I, pos );
+% figure(2)
+% imshow(imCp);
+arr = zeros(1,4);
+arr(1:4)=[1,2,3,4]
